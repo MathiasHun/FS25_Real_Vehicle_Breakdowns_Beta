054f7013e98a86a52b8110cf9b97d57efe758cfa
MathiasHU <30857803+MathiasHun@users.noreply.github.com>
2026-02-23 11:13:19 +0100

    v0.9.6.2

--- FS25_gameplay_Real_Vehicle_Breakdowns/scripts/vehicles/specializations/rvbWearable.lua
+++ FS25_gameplay_Real_Vehicle_Breakdowns/scripts/vehicles/specializations/rvbWearable.lua
@@ -1,30 +1,37 @@
 rvbWearable = {}
 
 function rvbWearable.updateDamageAmount(self, superFunc, dt)
-	local rvb = self.spec_faultData
-	if rvb and rvb.isrvbSpecEnabled then
-		local currentPartslifetime = 0
-		local currentPartsoperatingHours = 0
-		local ignoredParts = {
-			TIREFL = true, TIREFR = true, TIRERL = true, TIRERR = true
-		}
-		for _, key in ipairs(g_vehicleBreakdownsPartKeys) do
-			if rvb.parts ~= nil then
-				local part = rvb.parts[key]
-				if part and not ignoredParts[key] then
-					local maxLifetime = PartManager.getMaxPartLifetime(self, key)
-					currentPartslifetime = currentPartslifetime + maxLifetime
-					currentPartsoperatingHours = currentPartsoperatingHours + part.operatingHours
-				end
-			end
-		end
-		if currentPartslifetime > 0 then
-			self:setDamageAmount(currentPartsoperatingHours / currentPartslifetime)
-		else
-			self:setDamageAmount(0)
-		end
+    local rvb = self.spec_faultData
+	if not rvb or not rvb.isrvbSpecEnabled then
+        return superFunc(self, dt)
+    end
+	local motorSpec = self.spec_motorized
+	if not motorSpec or not self:getIsMotorStarted() then
 		return 0
-	else
-		return superFunc(self, dt)
 	end
+    local parts = rvb.parts
+	if not parts then
+		return 0
+	end
+    local partKeys = g_vehicleBreakdownsPartKeys
+    local tyreParts = TYRE_PARTS
+    --local getMaxLifetime = PartManager.getMaxPartLifetime
+    local totalLifetime = 0
+    local totalOperatingHours = 0
+    for i = 1, #partKeys do
+        local key = partKeys[i]
+        local part = parts[key]
+        if part and not tyreParts[key] then
+            --local maxLifetime = getMaxLifetime(self, key)
+			local maxLifetime = rvb.cachedMaxLifetime[key]
+            totalLifetime = totalLifetime + maxLifetime
+            totalOperatingHours = totalOperatingHours + part.operatingHours
+        end
+    end
+    local damage = 0
+    if totalLifetime > 0 then
+        damage = totalOperatingHours / totalLifetime
+    end
+    self:setDamageAmount(damage)
+    return 0
 end
