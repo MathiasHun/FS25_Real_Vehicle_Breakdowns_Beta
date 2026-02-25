aa5b7d6da2e13da762a7e835e90bd5981b124ed6
MathiasHU <30857803+MathiasHun@users.noreply.github.com>
2026-02-25 18:39:40 +0100

    Update rvbWearable.lua - v0.9.6.3

--- FS25_gameplay_Real_Vehicle_Breakdowns/scripts/vehicles/specializations/rvbWearable.lua
+++ FS25_gameplay_Real_Vehicle_Breakdowns/scripts/vehicles/specializations/rvbWearable.lua
@@ -2,27 +2,30 @@ rvbWearable = {}
 
 function rvbWearable.updateDamageAmount(self, superFunc, dt)
     local rvb = self.spec_faultData
-	if not rvb or not rvb.isrvbSpecEnabled then
+	if rvb == nil or not rvb.isrvbSpecEnabled then
         return superFunc(self, dt)
     end
 	local motorSpec = self.spec_motorized
-	if not motorSpec or not self:getIsMotorStarted() then
+	if not motorSpec then
 		return 0
 	end
+	if rvb.needsInitialDamageSync then
+        rvb.needsInitialDamageSync = false
+    elseif not self:getIsMotorStarted() then
+        return 0
+    end
     local parts = rvb.parts
 	if not parts then
 		return 0
 	end
     local partKeys = g_vehicleBreakdownsPartKeys
     local tyreParts = TYRE_PARTS
-    --local getMaxLifetime = PartManager.getMaxPartLifetime
     local totalLifetime = 0
     local totalOperatingHours = 0
     for i = 1, #partKeys do
         local key = partKeys[i]
         local part = parts[key]
         if part and not tyreParts[key] then
-            --local maxLifetime = getMaxLifetime(self, key)
 			local maxLifetime = rvb.cachedMaxLifetime[key]
             totalLifetime = totalLifetime + maxLifetime
             totalOperatingHours = totalOperatingHours + part.operatingHours
