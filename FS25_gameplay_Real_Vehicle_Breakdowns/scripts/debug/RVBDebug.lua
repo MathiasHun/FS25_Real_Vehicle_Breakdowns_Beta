44de6f6072583ea8802056332b26482b7238391f
MathiasHU <30857803+MathiasHun@users.noreply.github.com>
2026-02-25 13:11:52 +0100

    Update RVBDebug.lua - v0.9.6.3
    Improved debugger system: errors/warnings are always shown, info messages remain menu-toggleable. Function names are now included in log output for easier traceability.

--- FS25_gameplay_Real_Vehicle_Breakdowns/scripts/debug/RVBDebug.lua
+++ FS25_gameplay_Real_Vehicle_Breakdowns/scripts/debug/RVBDebug.lua
@@ -3,26 +3,28 @@ RVBDebug = {}
 RVBDebug_mt = {__index = RVBDebug}
 
 function RVBDebug.new(generalSettings)
-    local self = setmetatable({}, RVBDebug_mt)
-    self.generalSettings = generalSettings
-    self.generalSettings.showdebugisplay = self.generalSettings.showdebugisplay or false
-    return self
+	local self = setmetatable({}, RVBDebug_mt)
+	self.generalSettings = generalSettings
+	self.generalSettings.showdebugisplay = self.generalSettings.showdebugisplay or false
+	return self
 end
-function RVBDebug:log(level, fmt, ...)
-    if self.generalSettings.showdebugisplay then
-        local msg = string.format(fmt, ...)
-        if level == "ERROR" then
-            Logging.error("[RVB] " .. msg)
-        elseif level == "WARNING" then
-            Logging.warning("[RVB] " .. msg)
-        else
-            Logging.info("[RVB] " .. msg)
-        end
-    end
+function RVBDebug:log(level, funcName, fmt, ...)
+	local msg = string.format(fmt, ...)
+	funcName = funcName or "unknownFunc"
+	msg = string.format("[%s] %s", funcName, msg)
+	if level == "ERROR" then
+		Logging.error("[RVB] " .. msg)
+	elseif level == "WARNING" then
+		Logging.warning("[RVB] " .. msg)
+	else
+		if self.generalSettings.showdebugisplay then
+			Logging.info("[RVB] " .. msg)
+		end
+	end
 end
-function RVBDebug:info(fmt, ...) self:log("INFO", fmt, ...) end
-function RVBDebug:warning(fmt, ...) self:log("WARNING", fmt, ...) end
-function RVBDebug:error(fmt, ...) self:log("ERROR", fmt, ...) end
+function RVBDebug:info(funcName, fmt, ...) self:log("INFO", funcName, fmt, ...) end
+function RVBDebug:warning(funcName, fmt, ...) self:log("WARNING", funcName, fmt, ...) end
+function RVBDebug:error(funcName, fmt, ...) self:log("ERROR", funcName, fmt, ...) end
 
 --[[
 debugger = RVBDebug.new(generalSettings)
