48063610d5f3cc127210896181ef96dc35278d96
MathiasHU <30857803+MathiasHun@users.noreply.github.com>
2026-02-25 13:28:02 +0100

    Update RVBserviceManual_Event.lua - v0.9.6.3
    Updated debugger system

--- FS25_gameplay_Real_Vehicle_Breakdowns/scripts/vehicles/specializations/events/RVBserviceManual_Event.lua
+++ FS25_gameplay_Real_Vehicle_Breakdowns/scripts/vehicles/specializations/events/RVBserviceManual_Event.lua
@@ -1,63 +1,47 @@
 
-RVBserviceManual_Event = {}
-local mt = Class(RVBserviceManual_Event, Event)
-InitEventClass(RVBserviceManual_Event, "RVBserviceManual_Event")
+RVBServiceRequest_Event = {}
+local mt = Class(RVBServiceRequest_Event, Event)
+InitEventClass(RVBServiceRequest_Event, "RVBServiceRequest_Event")
 
-function RVBserviceManual_Event.emptyNew()
+function RVBServiceRequest_Event.emptyNew()
 	return Event.new(mt)
 end
-function RVBserviceManual_Event.new(vehicle, entry)
-	local self = RVBserviceManual_Event.emptyNew()
+function RVBServiceRequest_Event.new(vehicle, farmId)
+	local self = RVBServiceRequest_Event.emptyNew()
 	self.vehicle = vehicle
-	self.entry = entry or {}
+	self.farmId = farmId
 	return self
 end
-function RVBserviceManual_Event:readStream(streamId, connection)
+function RVBServiceRequest_Event:readStream(streamId, connection)
 	self.vehicle = NetworkUtil.readNodeObject(streamId)
-	self.entry = {}
-	self.entry.entryType = streamReadInt16(streamId)
-	self.entry.entryTime = streamReadInt16(streamId)
-	self.entry.operatingHours = streamReadFloat32(streamId)
-	self.entry.odometer = streamReadFloat32(streamId)
-	self.entry.resultKey = streamReadString(streamId)
-	local count = streamReadUInt8(streamId)
-    if count > 0 then
-        self.entry.errorList = {}
-        for i = 1, count do
-            table.insert(self.entry.errorList, streamReadString(streamId))
-        end
-    end
-	self.entry.cost = streamReadFloat32(streamId)
+	self.farmId = streamReadInt16(streamId)
 	self:run(connection)
 end
-function RVBserviceManual_Event:writeStream(streamId, connection)
+function RVBServiceRequest_Event:writeStream(streamId, connection)
 	NetworkUtil.writeNodeObject(streamId, self.vehicle)
-	streamWriteInt16(streamId, self.entry.entryType)
-	streamWriteInt16(streamId, self.entry.entryTime)
-	streamWriteFloat32(streamId, self.entry.operatingHours)
-	streamWriteFloat32(streamId, self.entry.odometer)
-	streamWriteString(streamId, self.entry.resultKey)
-	local errors = self.entry.errorList or {}
-    streamWriteUInt8(streamId, #errors)
-    for _, err in ipairs(errors) do
-        streamWriteString(streamId, err)
-    end
-	streamWriteFloat32(streamId, self.entry.cost)
+	streamWriteInt16(streamId, self.farmId)
 end
-function RVBserviceManual_Event:run(connection)
+--[[function RVBServiceRequest_Event:run(connection)
+	if g_server == nil then
+		return
+	end
 	if self.vehicle ~= nil and self.vehicle:getIsSynchronized() then
-		self.vehicle:SyncClientServer_serviceManual(self.entry)
-	end
-	if not connection:getIsServer() then
-		g_server:broadcastEvent(self, false, connection, self.vehicle)
+		self.vehicle:startService(self.farmId)
 	end
 end
-function RVBserviceManual_Event.sendEvent(vehicle, data, noEventSend)
-	if noEventSend == nil or noEventSend == false then
-		if g_server ~= nil then
-			g_server:broadcastEvent(RVBserviceManual_Event.new(vehicle, data), true, nil, vehicle)
-		else
-			g_client:getServerConnection():sendEvent(RVBserviceManual_Event.new(vehicle, data))
+function RVBServiceRequest_Event.sendEvent(vehicle, farmId)
+	if g_server ~= nil then
+		vehicle:startService(farmId)
+	else
+		g_client:getServerConnection():sendEvent(RVBServiceRequest_Event.new(vehicle, farmId))
+	end
+end]]
+function RVBServiceRequest_Event:run(connection)
+	if self.vehicle ~= nil and self.vehicle:getIsSynchronized() then
+		self.vehicle:startService(self.farmId)
+		self.vehicle.rvbDebugger:info("RVBServiceRequest_Event run", "startService on vehicle %s", self.vehicle:getFullName())
+		if not connection:getIsServer() then
+			g_server:broadcastEvent(self, nil, nil, self.vehicle)
 		end
 	end
 end
